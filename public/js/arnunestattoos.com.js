c65c7e2fe2168e87bb0c62e91f6046243ec83266
Sven Steinheißer <sven.steinheisser@googlemail.com>
2012-04-03 12:58:45 +0200

    Added pushState stuff and bigger image.

--- public/js/arnunestattoos.com.js
+++ public/js/arnunestattoos.com.js
@@ -1,12 +1,31 @@
+window.Site = {
+  pageClosed: function() {
+    if (History.enabled) {
+      History.back();
+    }
+  }
+};
+
+$.extend($.fancybox.defaults, {
+  autoSize: false
+});
+
 $(document).ready(function() {
 
-  $("img").on('click', function(event) {
+  $(".js-link").on('click', function(event) {
     event.preventDefault();
-    $.extend($.fancybox.defaults, {
-      autoSize: false
+
+    var page = $(this).data('page'),
+        selector = "#" + page
+    ;
+
+    $.fancybox( $(selector).html(), {
+      'afterClose': Site.pageClosed
     });
-    $.fancybox( $('.gallery').html());
+
+    if (History.enabled) {
+      History.pushState( {page: page}, $('h1', selector).html(), page );
+    }
   });
 
 });
-
